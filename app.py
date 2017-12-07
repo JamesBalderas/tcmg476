1bced1d10b9e7a77a15a7a11845e8cc3686684f3
Aaron Dees <Aaron551517@tamu.edu>
2017-12-07 16:55:46 -0600

    The app.py

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask, jsonify
-import hashlib
+from flask import Flask, request, jsonify 
+import os
+from hashlib import md5
+import json 
 import math
-import json
 import requests
+
+
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+	html = "<h3>Hello {name}!</h3>" \
+		   "<b>Hostname:</b> {hostname}<br/>" \
+		   "<b>Visits:</b> {visits}"
+	#return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+	return html.format(name='world', hostname='localhost', visits='who knows')
+
+#md5hash
+@app.route("/md5/<user_input>")
+def md5_resp(user_input):
+#def md5_resp():
+
+	#user_input = request.args.get('q', 'foobar')
+	resp = md5(user_input).hexdigest()
+	json_object = {
+		'input': user_input, 
+		'output': resp
+	}
+	return json.dumps(json_object)
+ 
+#factorial 
+def fact(n):
+	if n > 0:
+		return n * fact(n-1)
+	else: 
+		return 1
+
+@app.route("/factorial/<int:user_input>")
+def factorial_resp(user_input):
+	return jsonify(
+		input = user_input,
+		output = fact(user_input)
+			)
+
+
+#fibonacci
 def fib2(n):   # return Fibonacci series up to n
     result = []
     a, b = 0, 1
@@ -13,54 +53,38 @@ def fib2(n):   # return Fibonacci series up to n
         result.append(b)
         a, b = b, a+b
     return result
-def prime(z):
-    if int(z) > 1:
-        for i in range(2,int(z)):
-            if (int(z) % i) == 0:
-                return False
-        else:
-            return True
-    else:
-        return False      
 
-@app.route('/')
-def hi():
-    return "Hello world"
-
-
-@app.route('/md5/<input_string>')
-def md5(input_string):
-    return jsonify(
-        input = input_string, 
-        output = hashlib.md5(input_string.encode("utf-8")).hexdigest()
-    )
-    
-@app.route('/factorial/<input_integer>')
-def factorial(input_integer):
-    if int(input_integer) < 0:
-        return "Error: input integer is negative."
-    else:
-        return jsonify(
-            input = int(input_integer),
-            output = math.factorial(int(input_integer))
+@app.route("/fibonacci/<int:user_input>")
+def fibonacci_resp(user_input):
+	 if int(user_input) <= 0:
+	 	return "Error: input integer is negative "
+   	 else:
+		return jsonify(
+            input = int(user_input),
+            output = fib2(int(user_input))
         )
 
-@app.route('/fibonacci/<input_integer>')
-def fibonacci(input_integer):
-    if int(input_integer) <= 0:
-        return "Error: input integer is negative."
-    else:
-        return jsonify(
-            input = int(input_integer),
-            output = fib2(int(input_integer))
-        )
+#is_prime
+def is_prime(n):
+    if n % 2 == 0 and n > 2: 
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True	
 
-@app.route('/prime/<input_integer>')
-def is_prime(input_integer):
-    return jsonify(
-        input = int(input_integer),
-        output = bool(prime(input_integer))
-    )
+
+
+@app.route("/is-prime/<int:user_input>")
+def i_prime(user_input):
+	return jsonify(
+		input = user_input,
+		output = is_prime(user_input)
+		)
+
+
+
+
 
 url = "https://hooks.slack.com/services/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"
 @app.route('/slack_alert/<input_string>')
@@ -75,6 +99,24 @@ def slack_alert(input_string):
         input = input_string,
         output = result
     )
+	
 
-if __name__ == '__main__':
-    app.run(ip=0.0.0.0, port=5000)
+
+
+
+#@app.route("/kv-retrieve/<string:user_input>")
+
+
+
+
+
+#@app.route("/kv-")
+
+
+
+
+
+        
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
